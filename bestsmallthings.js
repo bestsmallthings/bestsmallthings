5624632db247ddcf57781f120c662f5effb33765
bestsmallthings <107030806+bestsmallthings@users.noreply.github.com>
2022-07-09 11:19:02 +0530

    Update bestsmallthings.js

--- bestsmallthings.js
+++ bestsmallthings.js
@@ -1,70 +1,81 @@
-      /**
-       * @param sizeFn {(thing:any)=>number}
-       * @param onBestSmallThingAddedFn {()=>void}    
-       * @param onBestSmallThingsImprovedFn {()=>void}    
-      */
-      export function configUpdateBestSmallThings(
-        sizeFn,
-        onBestSmallThingAddedFn,
-        onBestSmallThingsImprovedFn
-      ) {
-        size = sizeFn;
-        onBestSmallThingAdded = onBestSmallThingAddedFn;
-        onBestSmallThingsImproved = onBestSmallThingsImprovedFn;
-        lowestBadness = Infinity;
-        bestSmallThings = [];
-        bestSmallThingsCommonSize = -1;
-        updateBestSmallThings = updateBestSmallThings1;    
-      }
+/**
+ * @param sizeFn {(thing:any)=>number}
+ * @param onBestSmallThingAddedFn {()=>void}
+ * @param onBestSmallThingsImprovedFn {()=>void}
+ */
+export function configUpdateBestSmallThings(
+  sizeFn,
+  onBestSmallThingAddedFn,
+  onBestSmallThingsImprovedFn
+) {
+  size = sizeFn;
+  onBestSmallThingAdded = onBestSmallThingAddedFn;
+  onBestSmallThingsImproved = onBestSmallThingsImprovedFn;
+  lowestBadness = Infinity;
+  assert(!dbgAlreadyInited);
+  dbgAlreadyInited = true;
+  bestSmallThings = [];
+  bestSmallThingsCommonSize = -1;
+  updateBestSmallThings = updateBestSmallThings1;
+}
 
-      /**
-       * @param thing {any}
-       * @param badness {number}
-       */
-      function updateBestSmallThings1(thing, badness) {
-        if (bestSmallThings.length != 0) {
-            throw new Error('internal state error!');  
-        }
-        bestSmallThingsCommonSize = size(thing);
-        lowestBadness = badness;
-        addToBestSmallThings(thing);
-        updateBestSmallThings = updateBestSmallThings2;
-      }
+function assert(x){if(!x)throw new Error('assertion failed');}
 
-      function updateBestSmallThings2(thing, badness) {
-        if (badness > lowestBadness) {
-          return;
-        }
-        const sizeOfThing = size(thing);    
-        if(badness < lowestBadness) {
-          lowestBadness = badness;
-          improveBestSmallThings(thing, sizeOfThing);
-          return;
-        }
-        if (sizeOfThing < bestSmallThingsCommonSize) {
-          improveBestSmallThings(thing, sizeOfThing);
-          return;
-        }
-        if (sizeOfThing === bestSmallThingsCommonSize) {
-          addToBestSmallThings(thing);
-        }
-      }
+let dbgAlreadyInited = false;
 
-      export let updateBestSmallThings = updateBestSmallThings1;
-      export let lowestBadness = Infinity;
-      export let bestSmallThings = [];
-      export let bestSmallThingsCommonSize = -1;
-      let size;
-      let onBestSmallThingAdded;
-      let onBestSmallThingsImproved;
+/**
+ * @param thing {any}
+ * @param badness {number}
+ */
+function updateBestSmallThings1(thing, badness) {
+  assert(dbgAlreadyInited);
+  assert (bestSmallThings.length == 0);
+  bestSmallThingsCommonSize = size(thing);
+  lowestBadness = badness;
+  addToBestSmallThings(thing);
+  updateBestSmallThings = updateBestSmallThings2;
+}
 
-      function addToBestSmallThings(thing) {
-          bestSmallThings.push(thing);
-          onBestSmallThingAdded();
-      }
+function updateBestSmallThings2(thing, badness) {
+  assert(dbgAlreadyInited);
+  if (badness > lowestBadness) {
+    return;
+  }
+  const sizeOfThing = size(thing);
+  if (badness < lowestBadness) {
+    lowestBadness = badness;
+    improveBestSmallThings(thing, sizeOfThing);
+    return;
+  }
+  if (sizeOfThing < bestSmallThingsCommonSize) {
+    improveBestSmallThings(thing, sizeOfThing);
+    return;
+  }
+  if (sizeOfThing === bestSmallThingsCommonSize) {
+    addToBestSmallThings(thing);
+  }
+}
 
-      function improveBestSmallThings(thing, sizeOfThing) {
-          bestSmallThingsCommonSize = sizeOfThing;   
-          bestSmallThings = [thing];
-          onBestSmallThingsImproved();
-      }
+export let updateBestSmallThings = updateBestSmallThings1;
+export let lowestBadness = Infinity;
+
+/**
+ * @type any[]
+ */
+export let bestSmallThings = [];
+
+export let bestSmallThingsCommonSize = -1;
+let size;
+let onBestSmallThingAdded;
+let onBestSmallThingsImproved;
+
+function addToBestSmallThings(thing) {
+  bestSmallThings.push(thing);
+  onBestSmallThingAdded();
+}
+
+function improveBestSmallThings(thing, sizeOfThing) {
+  bestSmallThingsCommonSize = sizeOfThing;
+  bestSmallThings = [thing];
+  onBestSmallThingsImproved();
+}
